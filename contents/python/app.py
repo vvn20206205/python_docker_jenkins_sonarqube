ef2fc56c8e4b8ebe74bc526fb0180d79d490dd72
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-03-11 12:01:47 +0700

    update app.py

--- contents/python/app.py
+++ contents/python/app.py
@@ -1,3 +1,4 @@
+# pip install Flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,3 +11,10 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+# http://127.0.0.1:5000/add/2/3
+# http://localhost:5000/add/2/3
+
+
+# http://127.0.0.1:5000
+# http://localhost:5000
